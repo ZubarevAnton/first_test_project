77a13922cbee8159345f096376c5362b09417576
anton <drifter5115@gmail.com>
2022-06-01 12:14:58 +0300

    adding SUCCESS_MESSAGE locatro

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_PAGE_TITLE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
+    BASKET_PRODUCT_TITLE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PRODUCT_PAGE_PRICE = (By.CSS_SELECTOR,".col-sm-6  > p.price_color")
+    BASKET_PRODUCT_PRICE = (By.CSS_SELECTOR, ".alertinner > p >strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
