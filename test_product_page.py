40b86ee5284dd88c8da90368c12214614be3709f
anton <drifter5115@gmail.com>
2022-06-06 13:30:40 +0300

    adding marks for review

--- test_product_page.py
+++ test_product_page.py
@@ -8,7 +8,6 @@ from pages.login_page import LoginPage
 
 # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
-@pytest.mark.login_guest
 class TestUsersAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -22,6 +21,7 @@ class TestUsersAddToBasketFromProductPage():
         page.register_new_user(email, password)
         page.should_be_autorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
         page = ProductPage(browser, link)
@@ -37,8 +37,10 @@ class TestUsersAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+
 @pytest.mark.parametrize('promo_code', ['0', '1', '2', '3', '4', '5', '6', pytest.param('7', marks=pytest.mark.xfail),
                                         '8', '9'])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, promo_code):
     link = (f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_code}")
     page = ProductPage(browser, link)
@@ -82,7 +84,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -90,7 +92,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
-@pytest.mark.new
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
